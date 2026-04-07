c2e5c331a984dc6c62928eafbe2349f51a11808b
P13a70N <takhellambampibaton@gmail.com>
2026-04-07 11:49:19 +0000

    lifting the state to app

--- JobDashboard/App.jsx
+++ JobDashboard/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import './styles.css';
 import JobForm from './JobForm.jsx';
 import JobList from './JobList.jsx';
 import Dashboard from './DashboardJob.jsx';
@@ -8,6 +9,8 @@ function App() {
   const [jobs, setJobs] = useState([]);
   const [showList, setShowList] = useState(false);
   const [showBoard, setShowBoard] = useState(false);
+   const [sort, setSort] = useState('');
+
   
   const handleAddJob = (job) => {
     setJobs((prev) => [...prev, job]);
@@ -17,22 +20,46 @@ function App() {
     setJobs((prev) =>prev.filter((job) => job.id !== id));
   };
   
+  const sortedJobs = [...jobs];
+   
+ if(sort === 'sortName'){
+   sortedJobs.sort((a,b) => b.companyName.localeCompare(a.companyName)) // desc
+ }
+ if(sort === 'sortStatus'){
+   sortedJobs.sort((a, b) => a.status.localeCompare(b.status))   // asc
+ }
+ if(sort === 'sortId'){
+   sortedJobs.sort((a,b) => a.id - b.id)  // asc
+ }
+  
  
  
   return (
     <>
     <JobForm onAddJob={handleAddJob} />
    
-    <button onClick={() => setShowList(!showList)}>
+    <button onClick={() => setShowList(!showList)} disabled={jobs.length === 0}>
     {showList ? "Hide list" : "Show list"}  
     </button>
     
-    {showList &&  <JobList jobs={jobs} onDelete={handleDelete} />}
+    {showList && 
+    <div>
+      Sort:
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <option value="" disabled>Select</option>
+        <option value="sortName">Name</option>
+        <option value="sortStatus">Status</option>
+        <option value="sortId">Id</option>
+      </select>
+      <br />
+      <JobList sortedJobs={sortedJobs} onDelete={handleDelete} />
+      </div>
+    }
    
     <button onClick={() => setShowBoard(!showBoard)}>
     {showBoard ? "Hide Board" : "Show Board"}  
     </button>
-    {showBoard && <Dashboard jobs={jobs} />}
+    {showBoard &&  <Dashboard jobs={jobs} />}
    
    
     </>
--- JobDashboard/JobList.jsx
+++ JobDashboard/JobList.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import {useState} from 'react';
 import JobCard from './JobCard.jsx';
-function JobList({ jobs, onDelete }){
- const [sort, setSort] = useState('');
- 
- const sortedJobs = [...jobs];
- 
- if(sort === 'sortName'){
-   sortedJobs.sort((a,b) => b.companyName.localeCompare(a.companyName)) // desc
- }
- if(sort === 'sortStatus'){
-   sortedJobs.sort((a, b) => a.status.localeCompare(b.status))   // asc
- }
- if(sort === 'sortId'){
-   sortedJobs.sort((a,b) => a - b)  // asc
- }
-  
+function JobList({ sortedJobs, onDelete }){
+
   return(
     <div>
-      Sort:
-      <select value={sort} onChange={(e) => setSort(e.target.value)}>
-        <option value="" disabled>Select</option>
-        <option value="sortName">Name</option>
-        <option value="sortStatus">Status</option>
-        <option value="sortId">Id</option>
-      </select>
-      <br />
-      
-      {sortedJobs.length === 0 ? <p>No jobs found</p> :
-           <ul>
+    {sortedJobs.length === 0 ?( <p>No jobs found</p>) :
+          ( <ul>
            {sortedJobs.map((job) =>(
             <JobCard 
              key={job.id}
@@ -36,10 +13,8 @@ function JobList({ jobs, onDelete }){
              onDelete={onDelete}
            />
           ))}
-        </ul>}
-      
+        </ul>)}
     </div>
-    
     );
 }
 
